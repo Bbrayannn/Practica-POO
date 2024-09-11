0618ae64eeabdc32575a4fadc62c811f92d32f81
Bbrayannn <narvaezbrayan45@gmail.com>
2024-09-10 23:37:47 -0500

    mi primer commit

---
+++ MiPrimerarappjava.java
@@ -0,0 +1,15 @@
+package POO2024a;
+
+
+import java.util.Scanner;// importa la clase de clase scanner  
+import javax.swing.*;// importo todas las libreria swing
+
+
+public class MiPrimerarappjava {
+    
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"hola mundo");
+    }
+    
+}
